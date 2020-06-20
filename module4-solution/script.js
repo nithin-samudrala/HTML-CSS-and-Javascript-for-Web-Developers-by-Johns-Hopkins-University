46787e7082515ebe80e0b8883247551def09a64b
nithin-samudrala <61551662+nithin-samudrala@users.noreply.github.com>
2020-06-20 17:40:11 +0530

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,19 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+    // Loop over the names array and say either 'Hello' or "Good Bye"
+    // using the 'speak' method or either helloSpeaker's or byeSpeaker's
+    // 'speak' method.
+    // See Lecture 50, part 1
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+})();
